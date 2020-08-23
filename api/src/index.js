1d0f1f36755d874149b11551a66b866a20a19abe
AASysoev <Chaegh2Oof4phok3yaph>
2020-08-23 22:36:32 +0300

    Initial commit

---
+++ api/src/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var port = require('../configuration').port;
+var connectDb = require('../helpers/db').connectDb;
+
+const startServer = () => {
+    app.listen(port);
+
+    const kittySchema = new mongoose.Schema({
+        name: String
+    });
+
+    const Kitten = mongoose.model('Kitten', kittySchema);
+
+    const newKitten = new Kitten({name: 'Habibasta'});
+
+    newKitten.save(() => {
+        Kitten.find((err, kittens) => {
+            console.log('FIND ALL KITTEN', kittens);
+        });
+    });
+};
+
+connectDb()
+    .on('error', console.log)
+    .on('disconnected', connectDb)
+    .once('open', startServer);
+
+app.get('/hello', (req, res) => {
+    res.json({status: 'API Microservice is working'});
+});
