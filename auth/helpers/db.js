b39f256b417270021bb16bca1c8cb2265a73a2a5
AASysoev <Chaegh2Oof4phok3yaph>
2020-08-25 00:53:56 +0300

    add frontend server

--- auth/helpers/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const db = require('./../configuration/index').db;
-
-module.exports.connectDb = () => {
-    mongoose.connect(db, {useNewUrlParser: true});
-
-    return mongoose.connection;
-};
